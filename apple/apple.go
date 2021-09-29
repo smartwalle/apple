cba475187f747e3f9b30b82a2805a271195045ec
smartwalle <smartwalle@gmail.com>
2021-09-29 16:52:09 +0800

    苹果内购调试完成

---
+++ apple/apple.go
@@ -0,0 +1,112 @@
+package apple
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+const (
+	kProductionURL = "https://buy.itunes.apple.com/verifyReceipt"
+	kSandboxURL    = "https://sandbox.itunes.apple.com/verifyReceipt"
+)
+
+var (
+	ErrBadReceipt = errors.New("bad receipt")
+)
+
+type Client struct {
+	client *http.Client
+}
+
+func New() *Client {
+	var c = &Client{}
+	c.client = http.DefaultClient
+	return c
+}
+
+// VerifyReceipt 验证苹果内购交易是否有效，
+// 首先请求苹果的服务器，获取票据(receipt)的详细信息，然后验证交易信息(transactionId)是否属于该票据，
+// 如果交易信息在票据中，则返回详细的交易信息。
+// 注意：本方法会先调用苹果生产环境接口进行票据查询，如果返回票据信息为测试环境中的信息时，则调用测试环境接口进行查询。
+func (this *Client) VerifyReceipt(transactionId, receipt string) (*Trade, *InApp, error) {
+	var trade, err = this.GetReceipt(receipt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// 没有交易信息
+	if trade == nil {
+		return nil, nil, ErrBadReceipt
+	}
+
+	// 票据查询失败
+	if trade.Status != 0 {
+		return nil, nil, fmt.Errorf("bad receipt: %d", trade.Status)
+	}
+
+	// 验证 transactionId 和 receipt 是否匹配
+	if trade.Receipt != nil {
+		for _, info := range trade.Receipt.InApp {
+			if info.TransactionId == transactionId {
+				return trade, info, nil
+			}
+		}
+	}
+	return nil, nil, ErrBadReceipt
+}
+
+// GetReceipt 获取票据信息
+// 注意：本方法会先调用苹果生产环境接口进行票据查询，如果返回票据信息为测试环境中的信息时，则调用测试环境接口进行查询。
+func (this *Client) GetReceipt(receipt string) (*Trade, error) {
+	// 从生产环境查询
+	var trade, err = this.request(kProductionURL, receipt)
+	if err != nil {
+		return nil, err
+	}
+
+	// 如果返回票据信息为测试环境中的信息时，则调用测试环境接口进行查询
+	if trade != nil && trade.Status == 21007 {
+		trade, err = this.request(kSandboxURL, receipt)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return trade, nil
+}
+
+func (this *Client) request(url string, receipt string) (*Trade, error) {
+	var p = &Param{}
+	p.Receipt = receipt
+
+	var data, err = json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+
+	var body = bytes.NewReader(data)
+
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	rsp, err := this.client.Do(req)
+	if rsp != nil && rsp.Body != nil {
+		defer rsp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var trade *Trade
+
+	var decoder = json.NewDecoder(rsp.Body)
+	if err = decoder.Decode(&trade); err != nil {
+		return nil, err
+	}
+
+	return trade, nil
+}
---
+++ apple/apple_type.go
@@ -0,0 +1,63 @@
+package apple
+
+// 21000 App Store 无法读取你提供的JSON数据
+// 21002 收据数据不符合格式
+// 21003 收据无法被验证
+// 21004 你提供的共享密钥和账户的共享密钥不一致
+// 21005 收据服务器当前不可用
+// 21006 收据是有效的，但订阅服务已经过期。当收到这个信息时，解码后的收据信息也包含在返回内容中
+// 21007 收据信息是测试用（sandbox），但却被发送到产品环境中验证
+// 21008 收据信息是产品环境中使用，但却被发送到测试环境中验证
+
+type Environment string
+
+const (
+	EnvironmentSandbox    = "Sandbox"
+	EnvironmentProduction = "Production"
+)
+
+type Param struct {
+	Receipt string `json:"receipt-data"`
+}
+
+type Trade struct {
+	Receipt     *Receipt    `json:"receipt"`
+	Environment Environment `json:"environment"`
+	Status      int         `json:"status"`
+}
+
+type Receipt struct {
+	ReceiptType                string   `json:"receipt_type"`
+	AdamId                     int64    `json:"adam_id"`
+	AppItemId                  int64    `json:"app_item_id"`
+	BundleId                   string   `json:"bundle_id"`
+	ApplicationVersion         string   `json:"application_version"`
+	DownloadId                 int64    `json:"download_id"`
+	VersionExternalIdentifier  int64    `json:"version_external_identifier"`
+	ReceiptCreationDate        string   `json:"receipt_creation_date"`
+	ReceiptCreationDateMS      string   `json:"receipt_creation_date_ms"`
+	ReceiptCreationDatePst     string   `json:"receipt_creation_date_pst"`
+	RequestDate                string   `json:"request_date"`
+	RequestDateMS              string   `json:"request_date_ms"`
+	RequestDatePst             string   `json:"request_date_pst"`
+	OriginalPurchaseDate       string   `json:"original_purchase_date"`
+	OriginalPurchaseDateMS     string   `json:"original_purchase_date_ms"`
+	OriginalPurchaseDatePst    string   `json:"original_purchase_date_pst"`
+	OriginalApplicationVersion string   `json:"original_application_version"`
+	InApp                      []*InApp `json:"in_app"`
+}
+
+type InApp struct {
+	Quantity                string `json:"quantity"`
+	ProductId               string `json:"product_id"`
+	TransactionId           string `json:"transaction_id"`
+	OriginalTransactionId   string `json:"original_transaction_id"`
+	PurchaseDate            string `json:"purchase_date"`
+	PurchaseDateMS          string `json:"purchase_date_ms"`
+	PurchaseDatePst         string `json:"purchase_date_pst"`
+	OriginalPurchaseDate    string `json:"original_purchase_date"`
+	OriginalPurchaseDateMS  string `json:"original_purchase_date_ms"`
+	OriginalPurchaseDatePst string `json:"original_purchase_date_pst"`
+	IsTrialPeriod           string `json:"is_trial_period"`
+	InAppOwnershipType      string `json:"in_app_ownership_type"`
+}
